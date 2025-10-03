5122ce684a76e9a1c9187dc320d3a1af769a0d3a
Chris Ellis <christopher.ellis12@snhu.edu>
2025-10-02 23:44:56 -0400

    Add login/logout routes and tie user model into app. Add shell context and sqlite config.

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,10 @@
 from flask import render_template, redirect, flash, url_for
 from ..task.service import task_service
 from .forms import LoginForm
+from flask_login import current_user, login_user, logout_user
+from app import db
+import sqlalchemy as sa
+from app.models import User
 
 from app.main import bp
 
@@ -15,8 +19,20 @@ def index():
 
 @bp.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('main.login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('main.index'))
     return render_template('login.html', title='Sign In', form=form)
+
+@bp.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
